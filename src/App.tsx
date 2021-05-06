bcb08bc3432ae4d479fb87696e3e35d8a70f80a2
Arimsingle <arim.coe60@gmail.com>
2021-05-07 03:29:38 +0700

    first deploy on github

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Nav from "./components/Nav";
+import { useSelector } from "react-redux";
+import { AppState } from "./redux/store";
+const App: React.FC = () => {
+  const handleThemeChanged = () => (toggleTheme ? "white-mode" : "dart-mode");
+  const toggleTheme = useSelector((state: AppState) => state.theme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={handleThemeChanged()}>
+      <header>
+        <Nav />
       </header>
     </div>
   );
-}
+};
 
 export default App;
